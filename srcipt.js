67454c058de687badb45ed70888768835a91b149
Harsh_saini <2006harshsaini@gmail.com>
2026-07-17 13:37:08 +0530

    add project files

---
+++ srcipt.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#element", {
+    strings: [
+        "C | C++ | Python",
+        "HTML | CSS | JavaScript",
+        "Frontend Web Development",
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+
+});
