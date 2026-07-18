ba7aedae9251cb530158b1602d2ccc6d9e5eac09
Harsh_saini <2006harshsaini@gmail.com>
2026-07-18 20:25:33 +0530

    update files

--- srcipt.js
+++ srcipt.js
@@ -1,3 +1,6 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
 var typed = new Typed("#element", {
     strings: [
         "C | C++ | Python",
@@ -10,3 +13,19 @@ var typed = new Typed("#element", {
     loop: true
 
 });
+
+
+
+function opentab(event, tabname){
+
+    for(let tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for(let tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
